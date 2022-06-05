1577477de403a2369554168d5d7d69d59b589a30
D6CHECKER <100846319+D6CHECKER@users.noreply.github.com>
2022-06-05 15:15:22 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+#This is just example
 import OPERATOR
+#this must be exist
 def script():
     try:
-        print(OPERATOR.HOW_TO_USE_THIS_PACKAGE())
-        print(OPERATOR.Copyright())
+        #your script here
+        def p():
+            t = OPERATOR.Copyright()
+            return t
+        print(p())
     except:
+        #your except script here
         print("")
 if __name__=='__main__':
+    #this must be exist
     OPERATOR.LocalForm()
